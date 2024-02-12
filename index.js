4a21c12f7a789072594b67d51e654702c680ec5c
siwasit <siwasit2546@gmail.com>
2024-02-12 14:52:09 +0700

    set route

--- index.js
+++ index.js
@@ -7,12 +7,16 @@ const educationRoute = require('./routes/education');
 const authRoute = require('./routes/login');
 const dormRoute = require('./routes/dormitory');
 const roomBookingRoute = require('./routes/roomBooking');
+const bookBookingRoute = require('./routes/library');
+const transportationRoute = require('./routes/transportation')
 
 app.use('/personal', personalRoute);
 app.use('/education', educationRoute);
 app.use('/auth', authRoute);
 app.use('/dorm', dormRoute);
 app.use('/room-booking', roomBookingRoute);
+app.use('/book-booking', bookBookingRoute);
+app.use('/bus-check', transportationRoute);
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
